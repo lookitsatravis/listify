12f27c40803e17dbced3b2f9f6ef0a0b4440916c
Travis Vignon <travis@lookitsatravis.com>
2014-02-27 20:24:10 -0600

    Updated case in test bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,24 +5,24 @@ require_once __DIR__ . '/test_app/vendor/autoload.php';
 function application_autoloader($class) {
     $class = strtolower($class);
     $class_filename = strtolower($class).'.php';
-    $class_root = dirname(__FILE__) . "/../src/lookitsatravis";
+    $class_root = dirname(__FILE__) . "/../src/Lookitsatravis";
 
     $directories = new RecursiveDirectoryIterator($class_root);
     foreach(new RecursiveIteratorIterator($directories) as $file) {
         
-        if (strtolower("lookitsatravis\\listify\\".$file->getFilename()) == $class_filename) {
+        if (strtolower("Lookitsatravis\\listify\\".$file->getFilename()) == $class_filename) {
             $full_path = $file->getRealPath();
             require_once $full_path;
             break;
         }
 
-        if (strtolower("lookitsatravis\\listify\\commands\\".$file->getFilename()) == $class_filename) {
+        if (strtolower("Lookitsatravis\\listify\\commands\\".$file->getFilename()) == $class_filename) {
             $full_path = $file->getRealPath();
             require_once $full_path;
             break;
         }
 
-        if (strtolower("lookitsatravis\\listify\\exceptions\\".$file->getFilename()) == $class_filename) {
+        if (strtolower("Lookitsatravis\\listify\\exceptions\\".$file->getFilename()) == $class_filename) {
             $full_path = $file->getRealPath();
             require_once $full_path;
             break;
